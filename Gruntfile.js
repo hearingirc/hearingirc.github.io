e4d6c2f0b8426dc09f24d6dd9700323ab9fc4f17
Tom Kersten <tom@whitespur.com>
2014-02-28 22:20:56 -0600

    + Added CSS minimization to deployment script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   grunt.initConfig({
     uncss: {
       options: {
@@ -8,6 +10,19 @@ module.exports = function(grunt) {
       dist: {
         src: "out/**/*.html",
         dest: "out/css/app.css"
+      }
+    },
+    cssmin: {
+      minify: {
+        options: {
+          keepSpecialComments: 0,
+          report: "min",
+          compatibility: "ie8"
+        },
+        files: {
+          "out/css/app.css": "out/css/app.css",
+          "out/css/wufoo.css": "out/css/wufoo.css"
+        }
       }
     }
   });
